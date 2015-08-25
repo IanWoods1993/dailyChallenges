2f31677eb26bf46ef8ea5174febd82a8e83ee9ce
IanWoods1993 <ianwoods1993@gmail.com>
2015-08-25 15:24:02 -0700

    solves the 1 input case

--- spiral.py
+++ spiral.py
@@ -1,43 +1,45 @@
 import math
+import pdb
 def main():
 	spiralSize = int(input("Spiral size: "))
 	pointNum = int(input("Point Number: "))
 	midPoint = math.ceil(spiralSize/2)
-	currPoint = [midPoint, midPoint]
-	print(currPoint)
-	i = 0
+	currPointLoc = [midPoint, midPoint]
+	currPointIndex = 1
+	radius = 0
 	right = True
-	left = False
-	down = False
-	up = False
-	while math.fabs(i) < math.ceil(spiralSize/2):
+	left  = False
+	down  = False
+	up    = False
+	#pdb.set_trace()
+	while abs(radius) < math.ceil(spiralSize/2):
+		if currPointIndex == pointNum:
+			print(currPointLoc)
 		if(right == True): #if we're moving right
-			i = i + 1
-			currPoint[0] = currPoint[0]+1 #increment X
-			if currPoint[0] == midPoint + math.fabs(i): #if we're as far right as we should be
+			currPointLoc[0] += 1 #increment X
+			if currPointLoc[0] > midPoint + abs(radius): #if we're as far right as we should be
 				right = False #stop moving right
+				radius += 1
 				up = True #start moving up
 				
 		elif(up == True): #if we're moving up
-			currPoint[1] = currPoint[1]-1 #decrement Y
-			if currPoint[1] == midPoint - math.fabs(i):
+			currPointLoc[1] -= 1 #decrement Y
+			if currPointLoc[1] == midPoint - abs(radius):
 				up = False #stop moving up
 				left = True #start moving left
 		
 		elif(left == True): #if we're moving left
-			currPoint[0] = currPoint[0]-1 #decrement x
-			if currPoint[0] == midPoint - math.fabs(i):
+			currPointLoc[0] -= 1 #decrement x
+			if currPointLoc[0] == midPoint - abs(radius):
 				left = False
 				down = True
 
 		elif(down == True):
-			currPoint[1] = currPoint[1] + 1
-			if currPoint[1] == midPoint + math.fabs(1):
+			currPointLoc[1] += 1
+			if currPointLoc[1] == midPoint + abs(radius):
 				down = False
 				right = True
-		
-		print(currPoint)
-		print("i is : ", i)
+		currPointIndex += 1
 	return
 
 main()
