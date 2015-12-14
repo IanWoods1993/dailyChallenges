236479918244979d808c7a99e99d075ac58b15a3
IanWoods1993 <ianwoods1993@gmail.com>
2015-12-14 11:28:53 -0800

    finds some solutions but not all

--- fruitBasket.py
+++ fruitBasket.py
@@ -4,31 +4,31 @@ import random
 CostsTable = {}
 def sumFruits(basket):
 	totalCost = 0
-	allFruits = [fruit[0] for fruit in basket]
-	appearances = {fruit: 0 for fruit in allFruits}
 	for fruit in basket:
-		appearances[fruit[0]] += 1 
-	for fruit in appearances:
-		print(fruit)
-		totalCost += appearances[fruit] * costs[fruit]
+		totalCost += CostsTable[fruit]
 	return totalCost
 
+def getAmtFruitInBasket(basket):
+	amounts = {fruit: 0 for fruit in basket}
+	for fruit in basket:
+		amounts[fruit] += 1
+	return amounts
+
+
 def placeFruit(basket):
-	print(basket)
-	if(sumFruits(basket) > 500): #if we're over the limit, return
+	currentCost = sumFruits(basket)
+	if currentCost > 500:
 		basket.pop()
-		return basket
-	elif(sumFruits(basket) < 500): #if we're under the limit
-		for fruit in costs:
-			if (sumFruits(basket) + costs[fruit]) > 500:
-			#if we have so many of one type of fruit we can't possibly use another
+		placeFruit(basket)
+	elif currentCost == 500:
+		print("SOLUTION: ", basket)
+	else:
+		for fruit in CostsTable:
+			if currentCost + CostsTable[fruit] > 500:
 				continue
 			else:
 				basket.append(fruit)
-		placeFruit(basket)
-	else: #we've exactly hit 500 - success breh
-		print(basket)
-		return basket
+				placeFruit(basket)
 				
 def main():
 	basketFile = open("fruitBasket.txt")
@@ -38,6 +38,7 @@ def main():
 		pair[1] = int(pair[1].strip("\n"))
 		fruit, cost = pair
 		CostsTable[fruit] = cost
+	print(CostsTable)
 	placeFruit(basket)
 	return
 main()
